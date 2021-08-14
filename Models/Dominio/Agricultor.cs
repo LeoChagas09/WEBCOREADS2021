d77807daff03dc34a6e35b602643a478fff2bc83
LeoChagas09 <LeoChagas09@hotmail.com>
2021-08-13 21:10:51 -0300

    Atualizado

---
+++ Models/Dominio/Agricultor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBCOREADS2021.Models.Dominio
+{
+    [Table("Agricultor")]
+    public class Agricultor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID")]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        [DisplayName("Proprietario")]
+        [Required(ErrorMessage ="Campo nome do proprietario é obrigatorio")]
+        public string proprietario { get; set; }
+        public string municipio { get; set; }
+        public string bairro { get; set; }
+        public int idade { get; set; }
+
+        public ICollection<Area> areas { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WEBCOREADS2021.Models;
 
 namespace WEBCOREADS2021
 {
@@ -23,6 +25,9 @@ namespace WEBCOREADS2021
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           services.AddDbContext<Contexto>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("WEBCOREADS2021")));
+
             services.AddControllersWithViews();
         }
 
